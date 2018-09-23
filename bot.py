fe0bd9b7b6a5f89c0a02de7e9de0c31494197539
Phn3X <stephan.taylor92@gmail.com>
2018-09-23 18:02:03 -0400

    other commit

--- bot.py
+++ bot.py
@@ -64,6 +64,7 @@ def get_message():
                 for item in split:
                     if item in replies:
                         reply_text=random.choice(replies[item])
+                        print(split)
                         return(reply_text)
 
 
